e1f1331f50337ba610203d50c1ad80c600d4a098
Stefan Hausotte <stefan.hausotte@gmx.de>
2025-01-26 18:10:21 +0100

    add two numbers in asm

--- libasm/helloworld.s
+++ libasm/helloworld.s
@@ -6,21 +6,48 @@
 // X16 - Mach System Call function number
 //
 
-.global _start			// Provide program starting address to linker
+.global _main			// Provide program starting address to linker
 .align 4			// Make sure everything is aligned properly
 
 // Setup the parameters to print hello world
 // and then call the Kernel to do it.
-_start: mov	X0, #1		// 1 = StdOut
+_main: mov	X0, #1		// 1 = StdOut
 	adr	X1, helloworld 	// string to print
 	mov	X2, #13	    	// length of our string
 	mov	X16, #4		// Unix write system call
 	svc	#0x80		// Call kernel to output the string
+
+	sub 	sp, sp, #16
+
+	mov	x0, #50
+	mov	x1, #3
+	bl	add
+
+	str	x0, [sp, #8]
+
+	mov	X0, #1		// 1 = StdOut
+	add	x1, sp, #8	// Load the string to print. PROBLEM: it's not a string, but an integer thus 50 + 3 = 53 is printed as ASCII 5
+	mov	X2, #8	    	// length of our string
+	mov	X16, #4		// Unix write system call
+	svc	#0x80		// Call kernel to output the string
+	add 	sp, sp, #16
+
 
 // Setup the parameters to exit the program
 // and then call the kernel to do it.
 	mov     X0, #0		// Use 0 return code
 	mov     X16, #1		// System call number 1 terminates this program
 	svc     #0x80		// Call kernel to terminate the program
+
+add:	sub	sp, sp, #16 
+	str	x0, [sp, #8]
+	str	x1, [sp]
+
+	ldr	x8, [sp, #8]
+	ldr	x9, [sp]
+	add	x0, x8, x9
+
+	add	sp, sp, #16
+	ret
 
 helloworld:      .ascii  "Hello World!\n"
